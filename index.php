5a5682db13c628d0ae01af93f7b83529a1f9d0f8
ntc-tp <info@ntc-tp.ru>
2017-07-02 23:43:19 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//add main Engine class
+require_once('engine/Engine.php');
+
+//initialize engine
+$MainEngine = new Engine();
+//run Engine
+$MainEngine->Run(); //all logic is located inside that method
+
+
+
